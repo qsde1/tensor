16d676199a1f9bd81274ea1cd528a886fb910e02
qsde1 <qsdwy1@gmail.com>
2024-04-26 17:14:42 +0500

    переделал mainLayout

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,11 +1,10 @@
-import { Component } from 'react';
+import { Component, cloneElement, renderItem } from 'react';
 import './MainLayout.css'
 import MyBoards from './Components/MyBoards/MyBoards'
 import Navbar from './Components/Navbar/Navbar';
 import HeaderBar from './Components/Header/HeaderBar';
 import { IconContext} from 'react-icons';
 import { IoMdClose, IoIosArrowForward } from "react-icons/io";
-
 
 export default class MainLayout extends Component {
     state = {
@@ -21,24 +20,29 @@ export default class MainLayout extends Component {
             <IconContext.Provider value={{color: 'rgb(40,176,217)', size: '1.5em'}}>
             <div className='main'>
                 <div className="header">
-                    <HeaderBar/>    
+                    {this.props.header}
                 </div>
-                <div className="container">
-                    {this.state.isHiddenSidebar &&
-                        <div
-                            className='container__open-button'
-                            onClick={this.toggleSidebar}
-                        >
-                            <IoIosArrowForward />
-                        </div>
-                    }
-                    {!this.state.isHiddenSidebar &&
-                        <div className="sidebar">
-                            <Navbar isHidden={this.state.isHiddenSidebar} handleNavbar={this.toggleSidebar}/>
-                        </div>
+                <div className="container">                    
+                    {this.props.sidebar &&
+                        <>
+                            {this.state.isHiddenSidebar &&
+                                <div
+                                    className='container__open-button'
+                                    onClick={this.toggleSidebar}
+                                >
+                                    <IoIosArrowForward />
+                                </div>
+                            }
+                            {!this.state.isHiddenSidebar &&
+                                <div className="sidebar">
+                                    {/* {не знаю как правильно передать props в компоненту, полученную из props, поэтому использую cloneElement} */}
+                                    {cloneElement(this.props.sidebar,{isHidden:this.state.isHiddenSidebar, handleNavbar:this.toggleSidebar})}
+                                </div>
+                            }
+                        </>
                     }
                     <div className="content">
-                        <MyBoards/>
+                        {this.props.content}
                     </div>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainLayout from './MainLayout'
+import MyBoards from './Components/MyBoards/MyBoards'
+import Navbar from './Components/Navbar/Navbar';
+import HeaderBar from './Components/Header/HeaderBar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainLayout />
+    <MainLayout
+      header={<HeaderBar/>}
+      sidebar={<Navbar/>}
+      content={<MyBoards/>}
+    />
   </React.StrictMode>,
 )
